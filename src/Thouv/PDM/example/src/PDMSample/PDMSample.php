fd45cb4ee9e21918d53fb1b21af9076d38515ac7
Kryphum <kryphum@gmail.com>
2017-12-23 14:48:44 -0500

    Formatting

--- src/Thouv/PDM/example/src/PDMSample/PDMSample.php
+++ src/Thouv/PDM/example/src/PDMSample/PDMSample.php
@@ -29,11 +29,15 @@ class PDMSample extends PluginBase implements Listener
         $player = $ev->getPlayer();
         $pdm_player = $this->pdm->getPlayer($player);
         if(!$pdm_player) {
-            echo "first join";
+            echo "first join\n";
             $pdm_player = $this->pdm->registerPlayer($player);
+        } else {
+            $prop = $this->pdm->getPlayer($player)->getProperty("last_joined");
+            echo $prop->getPropertyName() . ":" . $prop->getValue() . " ";
         }
         $pdm_player->updateProperty(PDMPropertyFactory::makeProperty("last_joined", time()), true);
-        echo $this->pdm->getPlayer($player)->getProperty("last_joined")->getValue();
+        $prop = $this->pdm->getPlayer($player)->getProperty("last_joined");
+        echo $prop->getPropertyName() . ":" . $prop->getValue();
     }
 
     public function onChat(PlayerChatEvent $ev)
