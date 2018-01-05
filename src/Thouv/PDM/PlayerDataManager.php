5e94a039957f7b62ad2186b582ddd200d090979e
Kryphum <kryphum@gmail.com>
2018-01-05 14:36:56 -0500

    fix instantiation of PDMPlayer

--- src/Thouv/PDM/PlayerDataManager.php
+++ src/Thouv/PDM/PlayerDataManager.php
@@ -65,7 +65,7 @@ final class PlayerDataManager extends PluginBase
 			return false;
 		}
 
-		$pdm_player = new PDMPlayer($player_name);
+		$pdm_player = new PDMPlayer($player_name, $sync);
 		$this->players[$player_name] = $pdm_player;
 
 		if($sync && $this->getProvider()) $this->getProvider()->registerPlayer($pdm_player);
